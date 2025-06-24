6c5d48d3896705cf5b0e33f1d5e65cb554ffd223
Nguyen Xuan Phong <ngxphong03@gmail.com>
2025-06-24 17:29:05 +0700

    [style]: Fix UI

--- src/components/header/contact-button.tsx
+++ src/components/header/contact-button.tsx
@@ -1,6 +1,7 @@
 'use client'
+
+import CustomBorderedButton from '@/components/bordered-button'
 import type {ILink} from '@/types/header.interface'
-import Image from 'next/image'
 import Link from 'next/link'
 
 interface ContactButtonProps {
@@ -10,19 +11,16 @@ interface ContactButtonProps {
 const ContactButton = ({contact}: ContactButtonProps) => {
   return (
     <Link
-      href={contact?.url ?? '#'}
-      className='flex-shrink-0 cursor-pointer h-[2.5rem] max-w-[24.1075rem] border border-solid border-[#1A1A1A] bg-[#1A1A1A] inline-flex items-center justify-center no-underline xsm:hidden py-[0.5rem] px-[1.5625rem] cursor-pointer'
+      href={contact.url}
+      className='flex-shrink-0 cursor-pointer h-[2.5rem] max-w-[24.1075rem] inline-flex items-center justify-center no-underline xsm:hidden'
     >
-      <span className='text-white text-[1rem] leading-[120%] font-normal mr-[0.75rem]'>
-        {contact?.title ?? ''}
-      </span>
-      <Image
-        alt=''
-        width={20}
-        height={20}
-        src='/images/arrowV1.svg'
-        className='w-[1.25rem] h-auto shrink-0'
-      />
+      <CustomBorderedButton
+        color='#00D3D0'
+        borderColor='#000'
+        borderLine='rgb(134 132 132 / 10%)'
+      >
+        {contact.title}
+      </CustomBorderedButton>
     </Link>
   )
 }
